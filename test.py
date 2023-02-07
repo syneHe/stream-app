3a171bd9180908b3ed84bfd0f9a573516cd66a45
Jiutang He <65802395+syneHe@users.noreply.github.com>
2023-02-07 16:15:39 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+######################################################
+st.title('CogAgent')
+
+st.markdown('''
+**A KNOWLEDGE-ENHANCED TEXT REPRESENTATION TOOLKIT FOR NATURAL LANGUAGE UNDERSTANDING**
+''')
+
+
